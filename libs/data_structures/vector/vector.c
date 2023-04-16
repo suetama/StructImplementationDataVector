d55932abdabb81114abb4736b6857040f7039f66
suetama <egor.kotolopenko@gmail.com>
2023-04-16 18:29:34 +0300

    memory usage of vector

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -1,3 +1,29 @@
-//
-// Created by egork on 16.04.2023.
-//
+#include <stdio.h>
+#include <stdint.h>
+#include "vector.h"
+
+int main() {
+    vector v = createVector(5);
+    v.data[0] = 1;
+    v.data[1] = 2;
+    v.data[2] = 3;
+    v.data[3] = 4;
+    v.data[4] = 5;
+
+    printVector(&v);
+
+    reserve(&v, 3);
+
+    printVector1(&v);
+
+    shrinkToFit(&v);
+
+    printVector1(&v);
+
+    deleteVector(&v);
+
+    printVector(&v);
+
+    return 0;
+}
+
--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -1,8 +1,89 @@
-//
-// Created by egork on 16.04.2023.
-//
-
 #ifndef VECTOR_VECTOR_H
 #define VECTOR_VECTOR_H
+
+#include <corecrt.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct vector {
+    int *data; // указатель на элементы вектора
+    size_t size; // размер вектора
+    size_t capacity; // вместимость вектора
+} vector;
+
+
+vector createVector(size_t n) {
+    vector *v = (vector*)malloc(sizeof(vector));  // выделяем память под структуру
+    if (v == NULL) {  // проверяем, удалось ли выделить память
+        fprintf(stderr, "bad alloc");  // выводим сообщение об ошибке в поток ошибок
+        exit(1);                       // завершаем программу с кодом 1
+    }
+    if (n == 0) {  // если размер вектора равен 0, то емкость также равна 0
+        v->data = NULL;  // указатель на данные вектора равен NULL
+        v->size = 0;
+        v->capacity = 0;
+    } else {
+        v->data = (int*)malloc(n * sizeof(int));  // выделяем память под массив данных
+        if (v->data == NULL) {  // проверяем, удалось ли выделить память
+            fprintf(stderr, "bad alloc");  // выводим сообщение об ошибке в поток ошибок
+            exit(1);                       // завершаем программу с кодом 1
+        }
+        v->size = n;       // устанавливаем размер вектора
+        v->capacity = n;   // устанавливаем емкость вектора
+    }
+
+    return *v;              // возвращаем дескриптор вектора
+}
+
+void printVector(vector *v) {
+    for (size_t i = 0; i < v->size; i++) {
+        printf("%d ", v->data[i]);
+    }
+    printf("\n");
+}
+
+void printVector1(vector *v) {
+    for (size_t i = 0; i < v->capacity; i++) {
+        printf("%d ", v->data[i]);
+    }
+    printf("\n");
+}
+
+void reserve(vector *v, size_t newCapacity) {
+    if (newCapacity == 0) {
+        free(v->data);  // если newCapacity == 0, то освобождаем память, выделенную под массив данных
+        v->data = NULL;
+        v->size = 0;
+        v->capacity = 0;
+    } else if (newCapacity > v->capacity) {
+        int *newData = (int*)realloc(v->data, newCapacity * sizeof(int));  // перевыделяем память под массив данных
+        if (newData == NULL) {  // проверяем, удалось ли перевыделить память
+            fprintf(stderr, "bad alloc");  // выводим сообщение об ошибке в поток ошибок
+            exit(1);                       // завершаем программу с кодом 1
+        }
+        v->data = newData;
+        v->capacity = newCapacity;
+    } else if (newCapacity < v->size) {
+        v->size = newCapacity;  // уменьшаем размер вектора до newCapacity
+    }
+}
+
+void clear(vector *v){
+    v->size = 0;
+}
+
+void shrinkToFit(vector *v){
+    v->capacity = v->size;
+    v->data = realloc(v->data, v->size * sizeof(int));
+}
+
+void deleteVector(vector *v) {
+    free(v->data);
+    v->data = NULL;
+    v->size = 0;
+    v->capacity = 0;
+}
+
+
 
 #endif //VECTOR_VECTOR_H
